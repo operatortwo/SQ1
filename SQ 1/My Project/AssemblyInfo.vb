aa056a4f33750ed9ccddebac02247d592bd3e80c
op2 <operatortwo@bluewin.ch>
2023-09-14 10:41:32 +0200

    duc 1.0.2.1

--- SQ 1/My Project/AssemblyInfo.vb
+++ SQ 1/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' indem Sie "*" wie unten gezeigt eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.3.1")>
-<Assembly: AssemblyFileVersion("1.0.3.1")>
+<Assembly: AssemblyVersion("1.0.3.2")>
+<Assembly: AssemblyFileVersion("1.0.3.2")>
